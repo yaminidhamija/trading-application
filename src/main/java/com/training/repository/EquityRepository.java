0c09b3fb45c978fdca46c9bf63bb3c39e1d5955f
yaminidhamija <yaminidhamija@gmail.com>
2024-04-11 15:41:23 +0530

    fix comm. issue

--- src/main/java/com/training/repository/EquityRepository.java
+++ src/main/java/com/training/repository/EquityRepository.java
@@ -1,7 +1,7 @@
-package com.training.repository;
-
-import com.training.entity.Equity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EquityRepository extends JpaRepository<Equity,Integer> {
-}
+//package com.training.repository;
+//
+//import com.training.entity.Equity;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//
+//public interface EquityRepository extends JpaRepository<Equity,Integer> {
+//}
--- src/main/java/com/training/repository/TradeHoldingRepository.java
+++ src/main/java/com/training/repository/TradeHoldingRepository.java
@@ -1,16 +1,16 @@
-package com.training.repository;
-
-import com.training.entity.TradeHolding;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-public interface TradeHoldingRepository extends JpaRepository<TradeHolding,Integer> {
-
-    @Query("select th from TradeHolding th where th.trader.id = ?1 and th.equity.id = ?2")
-    public TradeHolding findByTraderIdAndEquityId(Integer traderId, Integer equityId);
-
-    @Query("select th from TradeHolding th where th.trader.id = ?1")
-    public List<TradeHolding> findByTraderId(Integer traderId);
-}
+//package com.training.repository;
+//
+//import com.training.entity.TradeHolding;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//
+//import java.util.List;
+//
+//public interface TradeHoldingRepository extends JpaRepository<TradeHolding,Integer> {
+//
+//    @Query("select th from TradeHolding th where th.trader.id = ?1 and th.equity.id = ?2")
+//    public TradeHolding findByTraderIdAndEquityId(Integer traderId, Integer equityId);
+//
+//    @Query("select th from TradeHolding th where th.trader.id = ?1")
+//    public List<TradeHolding> findByTraderId(Integer traderId);
+//}
